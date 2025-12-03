3c023ed01d1b9eee3ebf84b800dfbf41e8c6d427
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-12-03 18:55:56 +0600

    add

---
+++ practice/src/practice/Practice.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practice;
+
+/**
+ *
+ * @author HP
+ */
+public class Practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //while loop
+//        int i= 1;
+//        while(i<=5){
+//        
+//            System.out.println("I = "+i);
+//            i++;
+//        }
+
+        //for loop
+        for (int i = 12; i >= 1; i--) {
+            System.out.println("i = " + i);
+
+        }
+
+        for (int j = 1; j <= 15; j++) {
+//            System.out.println(j);
+            System.out.println("Haglu babu " + j);
+        }
+
+        
+        int day = 1;
+        switch(day){
+            case 1:
+                System.out.println("Eid Ul adha");
+                break;
+            case 2:
+                System.out.println("Eid ul Fitar");
+                break;
+            default:
+                System.out.println("Invalide Day");
+        
+        }
+        
+        
+       int salary = 20000;
+       
+       if(salary >= 20000)
+       {
+           System.out.println("I am interested");
+       }else{
+       System.out.println("I have no interest to work Here");
+       } 
+
+
+       
+//        last
+    }
+
+}
